6029445ec0db9af00e5ad9f90b4fb67b487ef230
cschank <chris@schankula.ca>
2017-02-24 15:35:42 -0500

    comments

--- ShiftReg.cpp
+++ ShiftReg.cpp
@@ -1,10 +1,12 @@
 #include "Arduino.h"
 #include "ShiftReg.h"
 
+//Shift Register Constructor
 ShiftReg::ShiftReg(){
-
+  
 }
 
+//needed at startup of program; sets number of shift registers and pins.
 ShiftReg::init(int n,int clockPin,int dataPin,int latchPin){
   pinMode(clockPin, OUTPUT);
   pinMode(dataPin, OUTPUT);
@@ -15,6 +17,7 @@ ShiftReg::init(int n,int clockPin,int dataPin,int latchPin){
   _latchPin = latchPin;
 }
 
+//Turn off all shift register switches.
 void ShiftReg::clearBytes() {
   for (int i = 0; i < _numSR; i++){
     _bytes[i] = B00000000;
@@ -22,16 +25,20 @@ void ShiftReg::clearBytes() {
   pushByte();
 }
 
+//return the current state of the shift register(s)
 byte ShiftReg::getByte(){
   return _bytes;
 }
 
+//set the current memory of the shift register, without pushing it to the register(s) itself.
+//probably not recommened, could be confusing. But may be useful in certain cases.
 void ShiftReg::setByte(byte bytes[]){
   for (int i = 0; i < _numSR; i++){
     _bytes[i] = bytes[i];
   }
 }
 
+//push the current bytes to the shift register(s)
 void ShiftReg::pushByte(){
   digitalWrite(_latchPin, LOW);
   for (int i = 0; i < _numSR; i++) {
@@ -40,7 +47,8 @@ void ShiftReg::pushByte(){
   }
   digitalWrite(_latchPin, 1);
 }
-    
+
+//set the current byte to a new value and push it to the shift register
 void ShiftReg::setPByte(byte bytes[]){
   setByte(bytes);
   pushByte();
